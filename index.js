c917bc023d9e574962ddeea73f6e81ea5e999c05
spikeburton <ralphburton3@gmail.com>
2019-07-15 21:13:09 -0400

    refactor to use express router

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 require('./services/passport');
 
 const app = express();
-require('./routes/authRoutes')(app);
+// require('./routes/authRoutes')(app);
+app.use('/auth', require('./routes/authRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,20 @@
+const router = require('express').Router();
 const passport = require('passport');
 
-module.exports = app => {
-  app.get(
-    '/auth/google',
-    passport.authenticate('google', { scope: ['profile', 'email'] })
-  );
+// module.exports = app => {
+//   app.get(
+//     '/auth/google',
+//     passport.authenticate('google', { scope: ['profile', 'email'] })
+//   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
-};
+//   app.get('/auth/google/callback', passport.authenticate('google'));
+// };
+
+router.get(
+  '/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get('/google/callback', passport.authenticate('google'));
+
+module.exports = router;
