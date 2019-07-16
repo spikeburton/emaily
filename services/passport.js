370edf01b85118561bb232b7764b1c347886e62e
spikeburton <ralphburton3@gmail.com>
2019-07-16 10:04:16 -0400

    add users model schema

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +13,20 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token: ', accessToken);
-      console.log('refresh token: ', refreshToken);
-      console.log('profile: ', profile);
+      // console.log('access token: ', accessToken);
+      // console.log('refresh token: ', refreshToken);
+      // console.log('profile: ', profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // We already have a record with given ID
+          done(null, existingUser);
+        } else {
+          // create a new user since it does not exist
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
