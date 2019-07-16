48d5ac585a9b759548115d8034d6caf1078474a0
spikeburton <ralphburton3@gmail.com>
2019-07-15 22:04:05 -0400

    fix mongoose warning

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 // require('./routes/authRoutes')(app);
