847dc44bd54647c98632be2920e4eec5255f70fc
spikeburton <ralphburton3@gmail.com>
2019-07-17 15:01:16 -0400

    remove bogus http request

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello, World!</h2>
+        <a href="/auth/google">Sign in with Google</a>
       </header>
     </div>
   );
