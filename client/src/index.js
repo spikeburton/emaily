10a23f36f62f571461546272457958260f2acfb1
spikeburton <ralphburton3@gmail.com>
2019-07-18 15:53:10 -0400

    add redux and reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,17 @@
 // Data layer
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,8 @@
+export default (state = {}, action) => {
+  const { type } = action;
+
+  switch (type) {
+    default:
+      return state;
+  }
+};
