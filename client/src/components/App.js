ed789bccedb412575e546fbdc63abab8b5d42259
spikeburton <ralphburton3@gmail.com>
2019-07-18 16:05:52 -0400

    add react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,11 @@
 // Render layer
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return <div>Hi There!</div>;
