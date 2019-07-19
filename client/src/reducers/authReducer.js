731d51ab83c30c3a5363b1302e1a475d9f2e1718
spikeburton <ralphburton3@gmail.com>
2019-07-19 00:42:44 -0400

    add FETCH_USER action type

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,20 @@
-export default (state = {}, action) => {
-  const { type } = action;
+import { FETCH_USER } from '../actions/types';
+
+const initialState = {
+  user: null,
+  loading: true
+};
+
+export default (state = initialState, action) => {
+  const { type, payload } = action;
 
   switch (type) {
+    case FETCH_USER:
+      return {
+        ...state,
+        user: payload,
+        loading: false
+      };
     default:
       return state;
   }
