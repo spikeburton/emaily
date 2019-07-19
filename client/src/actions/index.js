e850bf60aac32e34924e3bbfd76a9c967ead34cb
spikeburton <ralphburton3@gmail.com>
2019-07-19 00:33:40 -0400

    fetch current user on mount

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -11,7 +11,7 @@ export const fetchUser = () => {
         payload: res.data
       });
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 };
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,9 @@
 // Render layer
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { fetchUser } from '../actions';
 
 import Header from './Header';
 
@@ -8,19 +11,28 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div className="container">
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </Switch>
-      </Router>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="container">
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { fetchUser }
+)(App);
