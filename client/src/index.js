13f1bbdbc30f6ece99df4c859d1b347f1bcbeb7e
spikeburton <ralphburton3@gmail.com>
2019-07-18 20:11:38 -0400

    add materialize

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+import 'materialize-css/dist/css/materialize.min.css';
+
 import App from './components/App';
 import reducers from './reducers';
 
