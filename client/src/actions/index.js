4ba0639a82e0ffbba2d8d7e65543d96fe1f67495
spikeburton <ralphburton3@gmail.com>
2019-07-19 00:07:20 -0400

    add fetchUser action

---
+++ client/src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+export const fetchUser = () => {
+  return async dispatch => {
+    try {
+      const res = await axios.get('/auth/current_user');
+
+      dispatch({
+        type: FETCH_USER,
+        payload: res.data
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
