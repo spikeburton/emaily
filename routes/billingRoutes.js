3fa95a7b953cce9114bd150024a17bbcc4e902e4
spikeburton <ralphburton3@gmail.com>
2019-07-22 16:15:09 -0400

    send user data back in response

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -10,7 +10,10 @@ router.post('/', async (req, res) => {
     source: req.body.id
   });
 
-  console.log(charge);
+  req.user.credits += 5;
+  const user = await req.user.save();
+
+  res.send(user);
 });
 
 module.exports = router;
