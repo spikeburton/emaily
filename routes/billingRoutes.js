d22047335963cef191759be183ef770dd639de88
spikeburton <ralphburton3@gmail.com>
2019-07-22 16:08:05 -0400

    handle request to create a charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,8 +2,15 @@ const router = require('express').Router();
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
-router.post('/', (req, res) => {
-  console.log('STRIPE');
+router.post('/', async (req, res) => {
+  const charge = await stripe.charges.create({
+    amount: 500,
+    currency: 'usd',
+    description: '$5 for 5 credits',
+    source: req.body.id
+  });
+
+  console.log(charge);
 });
 
 module.exports = router;
