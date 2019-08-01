cf092f170633fe7de0d5b27d1258564aab642a04
spikeburton <ralphburton3@gmail.com>
2019-07-31 23:58:46 -0400

    add requireCredits middleware

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Emaily
 
 A full stack app built with Node/Express and React/Redux
+
+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
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,21 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
+const requireLogin = require('../middleware/requireLogin');
+const requireCredits = require('../middleware/requireCredits');
+
+const Survey = mongoose.model('surveys');
+
+router.post('/', [requireLogin, requireCredits], (req, res) => {
+  const { title, subject, body, recipients } = req.body;
+
+  const survey = new Survey({
+    title,
+    subject,
+    body,
+    recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+    _user: req.user.id,
+    dateSent: Date.now()
+  });
+});
 
 module.exports = router;
