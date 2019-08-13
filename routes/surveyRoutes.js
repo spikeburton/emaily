4829fb958c3dd7c07772292a129a3b86602a550f
spikeburton <ralphburton3@gmail.com>
2019-08-13 12:12:28 -0400

    integrate mailer into surveys route

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
-router.post('/', [requireLogin, requireCredits], (req, res) => {
+router.post('/', [requireLogin, requireCredits], async (req, res) => {
   const { title, subject, body, recipients } = req.body;
 
   const survey = new Survey({
@@ -20,7 +20,16 @@ router.post('/', [requireLogin, requireCredits], (req, res) => {
   });
 
   const mailer = new Mailer(survey, surveyTemplate(survey));
-  mailer.send();
+  try {
+    await mailer.send();
+    await survey.save();
+    req.user.credits -= 1;
+    const user = await req.user.save();
+
+    res.send(user);
+  } catch (err) {
+    res.status(422).send(err);
+  }
 });
 
 module.exports = router;
