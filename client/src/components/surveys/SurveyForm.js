3030d14c3b582e73aefd5d35ea3fdda053e9062b
spikeburton <ralphburton3@gmail.com>
2019-08-22 19:54:32 -0400

    add SurveyForm component

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class SurveyForm extends Component {
+  render() {
+    return (
+      <div>
+        <h3>SurveyForm!</h3>
+      </div>
+    );
+  }
+}
+
+export default SurveyForm;
